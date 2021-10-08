327b7cccb4fd2919f9056ce337246108b93293a2
Valdir Salustino <valdir.salustino@ibm.com>
2021-10-08 14:14:44 -0300

    refresh: Refresh some python decorators SUPPORTS (REST)
    - Remember some python decorators;
    
    Signed-off-by: Valdir Salustino <valdir.salustino@ibm.com>

--- python_snippets/simple_decorator.py
+++ python_snippets/simple_decorator.py
@@ -1,9 +1,10 @@
+import functools
+
+
 user = {'name': 'Jose', 'access_level': 'guest'}
 
-def get_admin_password():
-	return '1234'
-
 def make_secure(func):
+	@ functools.wraps(func)
 	def secure_function():
 		if user['access_level'] == 'admin':
 			return func()
@@ -11,6 +12,13 @@ def make_secure(func):
 			return f"{user['name']} does not have admin permisison"
 	return secure_function
 
-get_admin_password = make_secure(get_admin_password)
+@make_secure
+def get_admin_password():
+	return '1234'
+
+# get_admin_password = make_secure(get_admin_password)
 
 print(get_admin_password())
+print(get_admin_password.__name__)
+
+
